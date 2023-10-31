dca036eb4d58e7a3171a1c49a04c3bd7be455930
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-10-31 22:43:38 +0600

    navbar, logo: nav link and nav logo added.

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import "./NavBar.css";
+import { Link } from 'react-router-dom';
+import logo from "../../Resources/logos/Group 1329.png";
 
 const NavBar = () => {
     return (
         <div>
             <h1>NavBar</h1>
+            <nav>
+                <Link to="">
+                    <img src={logo} alt="Volunteer logo" srcset="" />
+                </Link>
+
+                <div>
+                    <Link to="/">Home</Link>
+                    <Link to="/donation">Donation</Link>
+                    <Link to="/events">Events</Link>
+                    <Link to="/blogs">Blogs</Link>
+                    <Link to="signin">Signin</Link>
+                    {/* <Link to=""></Link>
+                    <Link to=""></Link>
+                    <Link to=""></Link> */}
+                </div>
+            </nav>
         </div>
     );
 };
