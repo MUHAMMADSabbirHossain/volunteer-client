d35437f875a8f3e191df739148934fc4234f8336
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-10-31 23:05:37 +0600

    signin: signin component skeleton is create.

--- src/Components/Signin/Signin.js
+++ src/Components/Signin/Signin.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import "./Signin.css";
+import logo from "../../Resources/logos/Group 1329.png";
+import { Link } from 'react-router-dom';
 
 const Signin = () => {
     return (
         <div>
             <h1>Signin</h1>
+            <main>
+                <img src={logo} alt="Volunteer logo" sizes="" srcset="" />
+
+                <div>
+                    <h1>Login with</h1>
+                    <div>
+                        <img src="" alt="Google signin logo" sizes="" srcset="" />
+                        <p>Continue with Google</p>
+                    </div>
+                    <div>
+                        <p>Don't have an account? <span><Link to="/signup">Create an account free</Link></span>.</p>
+                    </div>
+                </div>
+            </main>
         </div>
     );
 };
