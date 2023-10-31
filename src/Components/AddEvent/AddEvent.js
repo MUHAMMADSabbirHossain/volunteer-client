40b13379c981d6b40450dbf43afff9795ff2c9cc
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-10-31 23:56:59 +0600

    addevent, volunteersreglist: skeleton / spelling

--- src/Components/AddEvent/AddEvent.js
+++ src/Components/AddEvent/AddEvent.js
@@ -5,6 +5,36 @@ const AddEvent = () => {
     return (
         <div>
             <h1>AddEvent</h1>
+            <main>
+                <h1>Add event</h1>
+
+                <section>
+
+                    <div>
+                        <h2>Volunteers register list</h2>
+                        <p>Add event</p>
+                    </div>
+                    <div>
+
+                        <form action="">
+
+                            <label htmlFor="eventTitle">Event Title: </label>
+                            <input type="text" name="eventTitle" id="" placeholder="Enter title" />
+
+                            <label htmlFor="eventDate">Event Date: </label>
+                            <input type="datetime-local" name="eventDate" id="" />
+
+                            <label htmlFor="description">Description</label>
+                            <textarea name="description" id="" cols="30" rows="10" placeholder="Enter designation"></textarea>
+
+                            <label htmlFor="banner">Banner: </label>
+                            <input type="text" name="banner" id="" placeholder="Banner links" />
+
+                            <button type="submit">Submit</button>
+                        </form>
+                    </div>
+                </section>
+            </main>
         </div>
     );
 };
--- src/Components/VolunteersRegList/VolunteersRegList.js
+++ src/Components/VolunteersRegList/VolunteersRegList.js
@@ -6,10 +6,10 @@ const volunteersRegList = () => {
         <div>
             <h1>volunteersRegList</h1>
             <main>
-                <h1>Volunteer Register list</h1>
+                <h1>Volunteers Register list</h1>
                 <section>
                     <div>
-                        <h2>Volunteer register list</h2>
+                        <h2>Volunteers register list</h2>
                         <p>Add event</p>
                     </div>
                     <div>
