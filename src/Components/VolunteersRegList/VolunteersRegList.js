1a7831e635601c5110d6383202f38d29187f45a1
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-10-31 23:41:24 +0600

    volunteersreglist: component skeleton is cretaed.

--- src/Components/VolunteersRegList/VolunteersRegList.js
+++ src/Components/VolunteersRegList/VolunteersRegList.js
@@ -5,6 +5,18 @@ const volunteersRegList = () => {
     return (
         <div>
             <h1>volunteersRegList</h1>
+            <main>
+                <h1>Volunteer Register list</h1>
+                <section>
+                    <div>
+                        <h2>Volunteer register list</h2>
+                        <p>Add event</p>
+                    </div>
+                    <div>
+                        <table></table>
+                    </div>
+                </section>
+            </main>
         </div>
     );
 };
