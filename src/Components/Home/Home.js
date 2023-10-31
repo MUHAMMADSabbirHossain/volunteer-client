450e061144d8310e823562e97b512a122c82d52f
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-10-31 22:56:35 +0600

    Home: home component skeleton is created.

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,6 +5,19 @@ const Home = () => {
     return (
         <div>
             <h1>Home</h1>
+            <main>
+                <h1>I grow by helping people in need.</h1>
+
+                <form action="">
+                    <input type="text" name="searchEvents" id="" placeholder="Serach events... " />
+                    <button type="submit">Search</button>
+                </form>
+
+                <div>
+
+                </div>
+
+            </main>
         </div>
     );
 };
