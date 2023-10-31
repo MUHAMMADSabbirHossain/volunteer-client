1e2d1ef56e4ba32a98b4f133b1b17cfcf40f989b
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-10-31 23:36:01 +0600

    events: component skeletion is created.

--- src/Components/Events/Events.js
+++ src/Components/Events/Events.js
@@ -4,6 +4,19 @@ const Events = () => {
     return (
         <div>
             <h1>Events</h1>
+
+            <main>
+
+                <h1>Event Tasks</h1>
+                <div>
+                    <div><img src="" alt="Event logo" sizes="" srcset="" /></div>
+                    <div>
+                        <h2>Humanity More</h2>
+                        <p>29 sep, 2020</p>
+                        <button type="submit">Cancel</button>
+                    </div>
+                </div>
+            </main>
         </div>
     );
 };
