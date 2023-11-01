8b34e1027cc36fc43a5390d4146ef148139ef757
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-11-01 23:27:53 +0600

    regasVolunteer: reg form handle with post data.

--- src/Components/RegAsVolunteer/RegAsVolunteer.js
+++ src/Components/RegAsVolunteer/RegAsVolunteer.js
@@ -3,6 +3,35 @@ import "./RegAsVolunteer";
 import logo from "../../Resources/logos/Group 1329.png";
 
 const RegAsVolunteer = () => {
+
+    // register as a volunteer
+    const handleVolunteerForm = event => {
+        event.preventDefault();
+
+        const fullName = event.target.fullName.value;
+        const email = event.target.email.value;
+        const dateTime = event.target.dateTime.value;
+        const description = event.target.description.value;
+        const eventTitle = event.target.eventTitle.value;
+
+        console.log(fullName, email, dateTime, description, eventTitle);
+
+        const volunteerDetails = { fullName, email, dateTime, description, eventTitle };
+
+        //post
+        const url = `http://localhost:5000/volunteers-detail`;
+        fetch(url, {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify(volunteerDetails)
+        })
+            .then(res => res.json())
+            .then(result => console.log(result));
+    };
+
+
     return (
         <div>
             <h1>RegAsVolunteer</h1>
@@ -13,22 +42,24 @@ const RegAsVolunteer = () => {
                 <div>
                     <h1>Register as a Volunteer</h1>
 
-                    <label htmlFor="fullName">Full Name: </label>
-                    <input type="text" name="fullName" id="" />
+                    <form onSubmit={handleVolunteerForm} action="">
+                        <label htmlFor="fullName">Full Name: </label>
+                        <input type="text" name="fullName" id="" />
 
-                    <label htmlFor="email">Username or Email: </label>
-                    <input type="text" name="email" id="" />
+                        <label htmlFor="email">Username or Email: </label>
+                        <input type="text" name="email" id="" />
 
-                    <label htmlFor="dateTime">Date and Time: </label>
-                    <input type="datetime-local" name="dateTime" id="" />
+                        <label htmlFor="dateTime">Date and Time: </label>
+                        <input type="datetime-local" name="dateTime" id="" />
 
-                    <label htmlFor="description">Description: </label>
-                    <textarea name="description" id="" cols="30" rows="10"></textarea>
+                        <label htmlFor="description">Description: </label>
+                        <textarea name="description" id="" cols="30" rows="10"></textarea>
 
-                    <label htmlFor="eventName">Event Name: </label>
-                    <input type="text" name="eventName" id="" />
+                        <label htmlFor="eventTitle">Event Title: </label>
+                        <input type="text" name="eventTitle" id="" />
 
-                    <button type="submit" >Submit</button>
+                        <button type="submit" >Submit</button>
+                    </form>
                 </div>
             </main>
         </div>
