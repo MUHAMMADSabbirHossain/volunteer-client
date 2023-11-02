f59d5375a09d20e2590c6030e784d4693e9325db
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-11-02 14:41:06 +0600

    navbar, style: initialy.

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -7,20 +7,20 @@ const NavBar = () => {
     return (
         <div>
             <h1>NavBar</h1>
-            <nav className="sm:flex justify-between px-10">
+            <nav className="font-semibold justify-around sm:flex">
                 <div>
                     <Link to="/">
-                        <img src={logo} alt="Volunteer logo" srcset="" width="20%" />
+                        <img src={logo} alt="Volunteer logo" srcset="" width="25%" />
                     </Link>
                 </div>
 
-                <div className="">
-                    <Link to="/">Home</Link>
-                    <Link to="/donation">Donation</Link>
-                    <Link to="/events">Events</Link>
-                    <Link to="/blogs">Blogs</Link>
-                    <Link to="/signin">Signin</Link>
-                    <Link to="/admin">Admin</Link>
+                <div className="flex flex-col sm:flex-row">
+                    <Link className="mx-1 px-1" to="/">Home</Link>
+                    <Link className="mx-1 px-1" to="/donation">Donation</Link>
+                    <Link className="mx-1 px-1" to="/events">Events</Link>
+                    <Link className="mx-1 px-1" to="/blogs">Blogs</Link>
+                    <Link className="mx-1 px-1" to="/signin">Signin</Link>
+                    <Link className="mx-1 px-1" to="/admin">Admin</Link>
                     {/* <Link to=""></Link>
                     <Link to=""></Link> */}
                 </div>
