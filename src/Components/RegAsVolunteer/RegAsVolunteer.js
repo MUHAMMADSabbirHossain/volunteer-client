6b6354e347a03db0ed7f543c5e164cbf463b5c8a
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-11-02 23:24:55 +0600

    RegAsVolunteer: form is styled.

--- src/Components/RegAsVolunteer/RegAsVolunteer.js
+++ src/Components/RegAsVolunteer/RegAsVolunteer.js
@@ -37,32 +37,33 @@ const RegAsVolunteer = () => {
             <h1>RegAsVolunteer</h1>
 
             <main>
-                <img src={logo} alt="" sizes="" srcset="" />
+                <img className="mx-auto my-10 " src={logo} alt="" sizes="" srcset="" width="25%" />
 
-                <div>
-                    <h1>Register as a Volunteer</h1>
+                <h1 className="text-2xl text-blue-500 sm:text-4xl">Register as a Volunteer</h1>
+
+                <div class="inline-block border-2 border-gray-200 rounded  py-4 px-8 my-6">
 
                     <form onSubmit={handleVolunteerForm} action="">
-                        <label htmlFor="fullName">Full Name: </label>
-                        <input type="text" name="fullName" id="" />
+                        <label className="block text-gray-500 font-bold text-left mb-1 md:mb-0 pr-4" for="grid-first-name" htmlFor="fullName">Full Name: </label>
+                        <input className=" appearance-none block w-80 bg-gray-200 text-gray-700 border border-blue-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="text" name="fullName" id="" />
 
-                        <label htmlFor="email">Username or Email: </label>
-                        <input type="text" name="email" id="" />
+                        <label className="block text-gray-500 font-bold text-left mb-1 md:mb-0 pr-4" for="grid-first-name" htmlFor="email">Username or Email: </label>
+                        <input className="appearance-none block w-80 bg-gray-200 text-gray-700 border border-blue-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="text" name="email" id="" />
 
-                        <label htmlFor="dateTime">Date and Time: </label>
-                        <input type="datetime-local" name="dateTime" id="" />
+                        <label className="block text-gray-500 font-bold text-left mb-1 md:mb-0 pr-4" for="grid-first-name" htmlFor="dateTime">Date and Time: </label>
+                        <input className="appearance-none block w-80 bg-gray-200 text-gray-700 border border-blue-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="datetime-local" name="dateTime" id="" />
 
-                        <label htmlFor="description">Description: </label>
-                        <textarea name="description" id="" cols="30" rows="10"></textarea>
+                        <label className="block text-gray-500 font-bold text-left mb-1 md:mb-0 pr-4" for="grid-first-name" htmlFor="description">Description: </label>
+                        <textarea className="appearance-none block w-80 bg-gray-200 text-gray-700 border border-blue-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" name="description" id="" cols="30" rows="5"></textarea>
 
-                        <label htmlFor="eventTitle">Event Title: </label>
-                        <input type="text" name="eventTitle" id="" />
+                        <label className="block text-gray-500 font-bold text-left mb-1 md:mb-0 pr-4" for="grid-first-name" htmlFor="eventTitle">Event Title: </label>
+                        <input className="appearance-none block w-80 bg-gray-200 text-gray-700 border border-blue-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="text" name="eventTitle" id="" />
 
-                        <button type="submit" >Submit</button>
+                        <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit" >Submit</button>
                     </form>
                 </div>
-            </main>
-        </div>
+            </main >
+        </div >
     );
 };
 
