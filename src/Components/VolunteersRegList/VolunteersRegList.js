35ceb7adfc991a1173fc60970e36a546bf682abc
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-11-02 11:38:48 +0600

    volunteersRegList: html added.

--- src/Components/VolunteersRegList/VolunteersRegList.js
+++ src/Components/VolunteersRegList/VolunteersRegList.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./VolunteersRegList.css";
 
 const VolunteersRegList = () => {
+
+    const [volunteersRegList, setVolunteersRegList] = useState([]);
+    useEffect(() => {
+        const url = `http://localhost:5000/volunteers-detail`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setVolunteersRegList(data));
+    }, []);
 
 
     return (
@@ -11,12 +19,44 @@ const VolunteersRegList = () => {
                 <h1>Volunteers Register list</h1>
                 <section>
                     <div>
-                        <h2>Volunteers register list</h2>
+                        <h2>Volunteers register list ({volunteersRegList.length})</h2>
                         <p>Add event</p>
                     </div>
+
                     <div>
-                        <table></table>
+
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>_id</th>
+                                    <th>Name</th>
+                                    <th>Email ID</th>
+                                    <th>Registating date</th>
+                                    <th>Volunteer list</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                {
+                                    volunteersRegList.map(volunteerRegList =>
+                                        <tr>
+                                            <td>{volunteerRegList._id} </td>
+                                            <td>{volunteerRegList.fullName}</td>
+                                            <td>{volunteerRegList.email}</td>
+                                            <td>{volunteerRegList.dateTime}</td>
+                                            <td>{volunteerRegList.eventTitle}</td>
+
+                                            {/* <td>{volunteerRegList.description}</td> */}
+                                            <td>X</td>
+                                        </tr>
+                                    )
+                                }
+                            </tbody>
+                        </table>
+
                     </div>
+
                 </section>
             </main>
         </div>
