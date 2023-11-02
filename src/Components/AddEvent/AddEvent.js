db9a2499f5a2288c3639451c210cf865afe5ac47
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-11-02 22:30:29 +0600

    AddEvent: form styled.

--- src/Components/AddEvent/AddEvent.js
+++ src/Components/AddEvent/AddEvent.js
@@ -42,21 +42,24 @@ const AddEvent = () => {
                     </div>
                     <div>
 
-                        <form onSubmit={handleAddEventForm} action="">
+                        <form className=" flex flex-col px-60" onSubmit={handleAddEventForm} action="">
 
-                            <label htmlFor="eventTitle">Event Title: </label>
-                            <input type="text" name="eventTitle" id="" placeholder="Enter title" />
+                            <label class="block text-gray-500 font-bold text-left mb-1 md:mb-0 pr-4" htmlFor="eventTitle">Event Title: </label>
+                            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="text" name="eventTitle" id="" placeholder="Enter title" />
 
-                            <label htmlFor="eventDate">Event Date: </label>
-                            <input type="datetime-local" name="eventDate" id="" />
+                            <label class="block text-gray-500 font-bold text-left mb-1 md:mb-0 pr-4" htmlFor="eventDate">Event Date: </label>
+                            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="datetime-local" name="eventDate" id="" />
 
-                            <label htmlFor="eventDescription">Description</label>
-                            <textarea name="eventDescription" id="" cols="30" rows="10" placeholder="Enter designation"></textarea>
 
-                            <label htmlFor="EventBanner">Banner: </label>
-                            <input type="text" name="eventBanner" id="" placeholder="Banner links" />
+                            <label class="block text-gray-500 font-bold text-left mb-1 md:mb-0 pr-4" htmlFor="eventDescription">Description: </label>
+                            <textarea className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" name="eventDescription" id="" cols="30" rows="5" placeholder="Enter designation"></textarea>
 
-                            <button type="submit">Submit</button>
+                            <label class="block text-gray-500 font-bold text-left mb-1 md:mb-0 pr-4" htmlFor="EventBanner">Banner: </label>
+                            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="text" name="eventBanner" id="" placeholder="Banner links" />
+
+                            <br />
+
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">Submit</button>
                         </form>
                     </div>
                 </section>
